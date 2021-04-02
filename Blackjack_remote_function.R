a47999a2af3cf9f263b2eb318c8c5294a5f378ce
bsiglow <bsiglow@nd.edu>
2021-04-02 14:03:30 -0400

    Remote function play

---
+++ Blackjack_remote_function.R
@@ -0,0 +1,13 @@
+load(url("https://www.dropbox.com/s/mixp2ttg0fujspf/BJ21_table_1?dl=1"))
+load(url("https://www.dropbox.com/s/ey6agxy1axv3r1t/BJ21_table_2?dl=1"))
+load(url("https://www.dropbox.com/s/xfwplo52lpdcg74/BJ21_table_3?dl=1"))
+
+
+
+load(url("https://www.dropbox.com/s/2vfq3eyb2qzj1vw/Black_jackplay?dl=1"))
+
+Black_jackplay(Your_hand_card1 = "ace",Your_hand_card2 = "JACK",Dealer_show = 7)
+
+
+
+
